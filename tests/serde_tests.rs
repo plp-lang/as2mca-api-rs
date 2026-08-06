6dc2b58b4477fe6fe6f3bae32bc5d88b674d53b4
Falldot <fe.offep@gmail.com>
2026-08-06 13:52:29 +1100

    test: `MethodValidate` serialize

--- tests/serde_tests.rs
+++ tests/serde_tests.rs
@@ -1,5 +1,5 @@
 use as2mca_api::{
-  requests::{CaseInsensitiveFilter, Filter, UserFilter, ViewDataGetCancelable},
+  requests::{CaseInsensitiveFilter, Filter, MethodValidate, UserFilter, ViewDataGetCancelable},
   responses::{ControlState, ControlsStates, MethodResult, Validate},
 };
 
@@ -160,4 +160,33 @@ fn test_se_de_view_data_get_cancelable() {
       "<ViewDataGetCancelable SessionID=\"4\" ViewShortName=\"4\" ClassID=\"4\" Hint=\"4\" AllowTimestampMilliseconds=\"false\"><UserFilter><AND><CaseInsensitiveFilter ColumnName=\"4\" Operator=\"=\" Value=\"4\"/></AND></UserFilter></ViewDataGetCancelable>"
     );
   }
+}
+
+#[test]
+fn test_se_de_method_validate() {
+  let res = MethodValidate {
+    session_id: "test",
+    method_id: 2_255_110_342,
+    r#type: as2mca_api::requests::ValidateType::Validate,
+    info: "%PLPCALL%",
+    do_commit: true,
+    get_debug_text: true,
+    optimized_grid_updates: true,
+    controls_states: vec![
+      as2mca_api::requests::ControlState {
+        id: 1_761_776_655,
+        value: "",
+      },
+      as2mca_api::requests::ControlState {
+        id: 1_761_776_748,
+        value: "",
+      },
+    ],
+    ..Default::default()
+  };
+  let xml = quick_xml::se::to_string(&res).unwrap();
+  assert_eq!(
+    xml,
+    "<MethodValidate SessionID=\"test\" MethodID=\"2255110342\" Type=\"VALIDATE\" Info=\"%PLPCALL%\" DoCommit=\"true\" GetDebugText=\"true\" OptimizedGridUpdates=\"true\"><ControlsStates><ControlState ID=\"1761776655\" Value=\"\"/><ControlState ID=\"1761776748\" Value=\"\"/></ControlsStates><PLPCallParameters/></MethodValidate>"
+  );
 }
